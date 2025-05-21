137dbdb170e9f24e7a1e940d7d799a7b19ae7d1e
PIDarifwidagdo <127385790+PIDarifwidagdo@users.noreply.github.com>
2025-05-21 15:54:24 +0700

    feat: draft CRUD tenant

---
+++ resources/views/components/btn-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['href' => '/', 'class' => 'inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary/75 focus:bg-primary/75 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</a>
--- resources/views/tenants/index.blade.php
+++ resources/views/tenants/index.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Dashboard') }}
+            {{ __('Tenants') }}
+            <x-btn-link class="float-right" href="{{ route('tenants.create') }}">Create Tenant</x-btn-link>
         </h2>
     </x-slot>
 
@@ -9,7 +10,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="flex flex-col p-6 text-gray-900 gap-y-2">
-                    {{ __("You're logged in!") }}
+                    {{-- {{ __("You're logged in!") }} --}}
                 </div>
             </div>
         </div>
