f852b34ddbef9467564c841fb76dd3fe2f6b597d
PIDarifwidagdo <127385790+PIDarifwidagdo@users.noreply.github.com>
2025-05-21 10:46:52 +0700

    feat: following discuss app

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,9 +18,9 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'discuss.test',
         '127.0.0.1',
         'localhost',
-        'discuss.test'
     ],
 
     /**
@@ -165,9 +165,9 @@ return [
      * understand which ones you want to enable.
      */
     'features' => [
-        // Stancl\Tenancy\Features\UserImpersonation::class,
+        Stancl\Tenancy\Features\UserImpersonation::class,
         // Stancl\Tenancy\Features\TelescopeTags::class,
-        // Stancl\Tenancy\Features\UniversalRoutes::class,
+        Stancl\Tenancy\Features\UniversalRoutes::class,
         // Stancl\Tenancy\Features\TenantConfig::class, // https://tenancyforlaravel.com/docs/v3/features/tenant-config
         // Stancl\Tenancy\Features\CrossDomainRedirect::class, // https://tenancyforlaravel.com/docs/v3/features/cross-domain-redirect
         // Stancl\Tenancy\Features\ViteBundler::class,
